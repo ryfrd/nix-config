8c8b1b67b6f2ce09b1d982a74ca3dcc8ee52fc59
robbygozzarder <jdysmcl@tutanota.com>
2023-06-28 12:35:16 +0100

    .

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -1,14 +1,25 @@
 { pkgs, ... }: {
   programs.tmux = {
+
     enable = true;
     clock24 = true;
     keyMode = "vi";
     mouse = true;
     shell = "\${pkgs.fish}/bin/fish";
 
+
     plugins = with pkgs; [
-      tmuxPlugins.cpu
-      tmuxPlugins.weather
+      {
+        plugin = tmuxPlugins.weather;
+        extraConfig = ''
+          set-option -g status-right "#{weather}"
+        '';
+      }
     ];
+
+    # extraConfig = ''
+    #   set -g status-left '#(uptime)'
+    # '';
+
   };
 }
