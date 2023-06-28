d8010621e614a87ee8197a921fc336a141c479ec
robbygozzarder <jdysmcl@tutanota.com>
2023-06-28 12:07:58 +0100

    .

--- home-manager/baron.nix
+++ home-manager/baron.nix
@@ -11,6 +11,7 @@
     ./gtk/de
     ./nvim/lazy
     ./syncthing
+    ./tmux
 
   ];
 
--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -5,5 +5,10 @@
     keyMode = "vi";
     mouse = true;
     shell = "\${pkgs.fish}/bin/fish";
+
+    plugins = with pkgs; [
+      tmuxPlugins.cpu
+      tmuxPlugins.weather
+    ];
   };
 }
