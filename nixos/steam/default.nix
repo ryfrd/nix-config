84b040e28339d650b818176c82b544a0bcd7c0d6
ryfrd <jdysmcl@tutanota.com>
2023-07-11 23:03:50 +0100

    .

--- nixos/steam/default.nix
+++ nixos/steam/default.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }: {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
@@ -11,22 +11,5 @@
     "steam-runtime"
     "steam-run"
   ];
-
-  programs.gamemode = {
-    enable = true;
-    enableRenice = true;
-    settings = {
-      general = {
-        renice = 10;
-      };
-      # Warning: GPU optimisations have the potential to damage hardware
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-    };
-  };
-
 }
 
