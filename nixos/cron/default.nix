d667f5d15ba6470631d8804d998e65b1db867cc2
ryfrd <jdysmcl@tutanota.com>
2023-07-12 15:18:10 +0100

    .

--- nixos/cron/default.nix
+++ nixos/cron/default.nix
@@ -1,13 +1,14 @@
-{ pkgs, ...}: {
+{ pkgs, ... }: {
   services.cron = {
     enable = true;
     systemCronJobs = [
-      # copy docker data from root drive to mount daily
       "@daily      root     sh /home/james/nix-config/nixos/cron/jobs/docker-data-backup.sh"
       "@daily      root     sh /home/james/nix-config/nixos/cron/jobs/smart.sh"
     ];
   };
-  environment.systemPackages = with pkgs; [ 
+
+  # dependencies of job scripts
+  environment.systemPackages = with pkgs; [
     gotify-cli
     smartmontools
   ];
--- nixos/cron/jobs/docker-updates.sh
+++
@@ -1,4 +0,0 @@
-repo_url=$(docker inspect "jellyfin" --format='{{.Config.Image}}')
-local_hash=$(docker image inspect "$repo_url" --format '{{.RepoDigests}}')
-
-echo $local_hash
