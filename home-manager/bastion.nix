32906e5624fc806e9faae30daa9e4266fbfac0e4
ryfrd <jdysmcl@tutanota.com>
2023-07-23 12:28:36 +0100

    .

---
+++ home-manager/bastion.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: {
+
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    ./base
+    ./cli
+    ./nvim/headless
+    ./tmux
+
+  ];
+
+}
---
+++ nixos/bastion.nix
@@ -0,0 +1,35 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
+
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-pc-ssd
+
+    ./hardware/bastion.nix
+
+    ./base
+    ./cron
+    ./docker
+    ./power
+
+  ];
+
+  # kernel
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  # networking
+  networking.hostName = "bastion";
+
+  # bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  fileSystems = {
+    "/".options = [ "compress=zstd" ];
+    "/home".options = [ "compress=zstd" ];
+    "/nix".options = [ "compress=zstd" "noatime" ];
+  };
+
+}
