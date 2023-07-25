93246c86e8c4a0802821ae3f2e4a2e73fb35e750
ryfrd <jdysmcl@tutanota.com>
2023-07-25 17:00:14 +0100

    .

--- pkgs/fetch/default.nix
+++ pkgs/fetch/default.nix
@@ -1,10 +1,24 @@
-{ stdenv, pkgs }:
+{ stdenv, pkgs, fetchFromGitHub, ... }:
 
 stdenv.mkDerivation {
   name = "fetch";
-  buildInputs = [
-    pkgs.python3
+
+  src = fetchFromGitHub {
+    owner = "ryfrd";
+    repo = "fetch";
+    rev = "bfc855819a3e90337d36e190dcaf209b1d51b32e";
+    sha256 = "1h0kflxjcfjrd9p3cgrzb4krjx3f5nid92alb1vjf8vx4jnb4byh";
+  };
+
+  buildInputs = with pkgs; [
+    python3
   ];
+
   dontUnpack = true;
-  installPhase = "install -Dm 0755 ${./fetch.py} $out/bin/fetch";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/fetch.py $out/bin/fetch
+    chmod +x $out/bin/fetch
+  '';
 }
