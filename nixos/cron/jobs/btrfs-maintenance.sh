a66d3f69194cd056bf1b93c46e81be86329e1fb8
ryfrd <jdysmcl@tutanota.com>
2023-08-20 16:31:55 +0100

    .

--- nixos/cron/jobs/btrfs-maintenance.sh
+++ nixos/cron/jobs/btrfs-maintenance.sh
@@ -1,7 +1,5 @@
 mount="/mnt/warhead"
 
 btrfs scrub start $mount && gotify push "scrub on $mount completed"
-btrfs balance start -musage=0 -dusage=0 $mount && gotify push "balance on $mount completed"
-#btrfs filesystem defragment -t 32m -f $mount && gotify push "defragment on $mount completed"
-fstrim $mount && gotify push "trim on $mount completed"
+btrfs balance start -musage=30 -dusage=50 $mount && gotify push "balance on $mount completed"
 
