6de3e72e66d1a10accc7cab2cbbc39cfbd28b126
ryfrd <jdysmcl@tutanota.com>
2023-08-21 15:33:43 +0100

    .

---
+++ nixos/reverse-proxy/bastion.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }: {
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "jdysmcl@tutanota.com";
+      dnsProvider = "cloudflare";
+      credentialsFile = "/etc/nixos/cloudflare.env";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 
+    80 443
+  ];
+
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts = {
+
+      "dns.dymc.win" = {
+        enableACME = true;
+        acmeRoot = null;
+        addSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          proxyWebsockets = true;
+        };
+      };
+
+      "home.dymc.win" = {
+        enableACME = true;
+        acmeRoot = null;
+        addSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8123";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+}
