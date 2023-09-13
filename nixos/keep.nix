d0aa34f4dbebed0764f569f2f02664e8d1d57f1b
ryfrd <jdysmcl@tutanota.com>
2023-09-13 11:20:17 +0100

    .

--- nixos/keep.nix
+++ nixos/keep.nix
@@ -58,7 +58,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "@weekly      james     sh /etc/cron-jobs/keep-backup.sh"
+      "@weekly      root     sh /etc/cron-jobs/keep-backup.sh"
       "@monthly      root     sh /etc/cron-jobs/btrfs-maintenance.sh"
     ];
   };
