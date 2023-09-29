76c4c422d8f152342168ab4712146423a2391a9b
ryfrd <jdysmcl@tutanota.com>
2023-09-29 12:20:27 +0100

    .

--- nixos/jobs/backup.sh
+++ nixos/jobs/backup.sh
@@ -1,4 +1,4 @@
-rsync -chavzP 'ssh -p 97' james@keep:/mnt/warhead/pics /home/james/backup/keep/pics
-rsync -chavzP 'ssh -p 97' james@keep:/mnt/warhead/docs /home/james/backup/keep/docs
-rsync -chavzP 'ssh -p 97' james@keep:/home/james/docker /home/james/backup/keep/docker
-rsync -chavzP 'ssh -p 97' james@phalanx:/home/james/docker /home/james/backup/phalanx/docker
+rsync -chavzP --delete -e 'ssh -p 97' james@keep:/mnt/warhead/pics /home/james/backup/keep
+rsync -chavzP --delete -e 'ssh -p 97' james@keep:/mnt/warhead/docs /home/james/backup/keep
+rsync -chavzP --delete -e 'ssh -p 97' james@keep:/home/james/docker /home/james/backup/keep
+rsync -chavzP --delete -e 'ssh -p 97' james@phalanx:/home/james/docker /home/james/backup/phalanx
