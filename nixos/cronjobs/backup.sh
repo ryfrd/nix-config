26e403f1e792f26f7daff32ec117025e3f2283be
ryfrd <jdysmcl@tutanota.com>
2024-03-24 11:29:13 +0000

    update

--- nixos/cronjobs/backup.sh
+++ nixos/cronjobs/backup.sh
@@ -1,12 +1,12 @@
 # pass directories to be backed up to hetzner as arguments eg.
 # sh backup.sh /home/james/pics /mnt/data/films
 
-remote="u370354@u370354.your-storagebox.de";
-i=1;
+remote="u370354@u370354.your-storagebox.de"
+i=1
 for dir in "$@" 
 do
-    rsync -a --delete -e 'ssh -p 23' $dir $remote:/home &&;
-    curl -d "back up of $dir to $remote succesful" https://ntfy.dymc.win/homelab ||;
+    rsync -a --delete -e 'ssh -p 23' $dir $remote:/home &&
+    curl -d "back up of $dir to $remote succesful" https://ntfy.dymc.win/homelab ||
     curl -d "back up of $dir to $remote failed" https://ntfy.dymc.win/homelab
-    i=$((i + 1));
+    i=$((i + 1))
 done
