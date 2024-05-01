c1ceb28dcd0cd9362de4eef63862365752cf9205
ryfrd <jdysmcl@tutanota.com>
2024-05-01 11:19:02 +0100

    zpool alert

--- nixos/homelab.nix
+++ nixos/homelab.nix
@@ -64,6 +64,7 @@
   ];
 
   # enable quicksync
+  boot.kernelParams = [ "i915.enable_guc=2" ];
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [ intel-media-driver intel-compute-runtime ];
