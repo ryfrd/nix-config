32a56b9277f63fa5562b349b85cf20e780622a66
ryfrd <jdysmcl@tutanota.com>
2024-06-10 12:19:07 +0200

    remotelab admin

--- nixos/features/headscale/default.nix
+++ nixos/features/headscale/default.nix
@@ -1,4 +1,5 @@
 {
+
   services.headscale = {
     enable = true;
     address = "0.0.0.0";
@@ -8,7 +9,7 @@
       db_type = "sqlite3";
       db_path = "/var/lib/headscale/db.sqlite";
       dns_config = {
-      	nameservers = [ "100.64.0.5" "1.1.1.1" ];
+      	nameservers = [ "1.1.1.1" ];
       	override_local_dns = true;
       };
     };
