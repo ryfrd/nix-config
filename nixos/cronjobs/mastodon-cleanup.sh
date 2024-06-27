56de6d7536f504d091cdc0b9e84b0699ee4d9a95
ryfrd <jdysmcl@tutanota.com>
2024-06-27 22:00:23 +0100

    remove mastodon script

--- nixos/cronjobs/mastodon-cleanup.sh
+++
@@ -1,4 +0,0 @@
-docker exec -it mastodon-web-1 tootctl media remove --days 1
-docker exec -it mastodon-web-1 tootctl media remove --days 1 --prune-profiles
-docker exec -it mastodon-web-1 tootctl media remove --days 1 --remove-headers
-docker exec -it mastodon-web-1 tootctl preview_cards remove --days 1
