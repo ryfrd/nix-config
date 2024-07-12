5fa71d2745767d6e4be12718ae30f1a2eb4974d0
ryfrd <jdysmcl@tutanota.com>
2024-07-12 14:27:37 +0200

    remotelab

--- nixos/remotelab.nix
+++ nixos/remotelab.nix
@@ -20,20 +20,6 @@
 
   time.timeZone = "Europe/Berlin";
 
-  services.cron = {
-    enable = true;
-    systemCronJobs =
-      [ "@daily	root	sh /etc/cronjobs/backup.sh /home/james/phalanx-docker" ];
-  };
-
-  environment.etc = {
-    "cronjobs/backup.sh" = { source = ./cronjobs/backup.sh; };
-  };
-
-  # cronjob deps
-  # required system wide as jobs run as root
-  environment.systemPackages = with pkgs; [ rsync ];
-
   # limit size of system journal
   services.journald.extraConfig = ''
     SystemMaxUse=100M
