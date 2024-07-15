1a0696dde33b2b971579c72efafac610e36626ca
ryfrd <jdysmcl@tutanota.com>
2024-07-15 12:39:09 +0100

    backup

---
+++ nixos/cronjobs/local-backup.sh
@@ -0,0 +1,23 @@
+# homelab wakes me up 1 hour before this script runs
+
+# pull backup from homelab
+syncoid -r james@homelab:warhead/high-prio warhead/homelab-backup --delete-target-snapshots --no-privilege-elevation --sshport=97 --sshkey=/root/.ssh/id_ed25519 &&
+curl -d "backup happy :)" ntfy.dymc.win/homelab ||
+curl -d "backup unhappy :(" ntfy.dymc.win/homelab
+
+# srub backup
+zpool scrub warhead
+
+# wait for scrub to finish
+while zpool status warhead | grep -q "scan: *scrub in progress"; do
+  echo "scrub still scrubbing"
+  sleep 60
+done
+
+# send notif
+curl -d "scrubbed warhead\n$(zpool status)\ngoing to sleep now" ntfy.dymc.win/homelab
+
+# go to sleep
+systemctl suspend
+
+# homelab will wake me up in a week
